46b957f179af39b587f944603fa4d4b65dd4c771
Yasmin Hillis <jasminehillis94@gmail.com>
2019-09-20 17:46:03 +0300

    adding the svg

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const svg = d3
             .select('#chart')
             .append('svg')
             .attr('width', svgWidth)
-            .attr('height', svgHeight)
+            .attr('height', svgHeight);
 
 const bottomLine = 300
 const circleRadius = 30
